8b2d29e3ef69322f6fc54a68da923f7d4fa17faf
里夫 <ziv.chan@softbi.com>
2020-04-21 12:00:25 +0800

    init: init all file

---
+++ src/test/java/com/ziv/mypra/MypraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ziv.mypra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypraApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
