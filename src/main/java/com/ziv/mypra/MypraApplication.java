2b1c073942a2ec79a761d23492752c30a8948c64
里夫 <ziv.chan@softbi.com>
2020-04-22 12:03:53 +0800

    refactor: 修改checkstyle掃瞄出的問題

--- src/main/java/com/ziv/mypra/MypraApplication.java
+++ src/main/java/com/ziv/mypra/MypraApplication.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MypraApplication {
 
+    /**
+     * 使用main方法進入
+     *
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(MypraApplication.class, args);
     }
--- src/main/java/com/ziv/mypra/web/GreetingWebApi.java
+++ src/main/java/com/ziv/mypra/web/GreetingWebApi.java
@@ -18,4 +18,3 @@ public class GreetingWebApi {
         return greetingService.greeting("test");
     }
 }
- 
