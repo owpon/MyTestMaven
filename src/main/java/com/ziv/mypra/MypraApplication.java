6e6a414feeb3e8316dd3248c782728bc3ab78952
里夫 <ziv.chan@softbi.com>
2020-04-23 09:46:43 +0800

    fix: 修改springboot 預設從jar 變war

--- src/main/java/com/ziv/mypra/MypraApplication.java
+++ src/main/java/com/ziv/mypra/MypraApplication.java
@@ -3,15 +3,16 @@ package com.ziv.mypra;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
-public class MypraApplication {
+public class MypraApplication extends SpringBootServletInitializer {
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(MypraApplication.class);
+    }
 
-    /**
-     * 使用main方法進入
-     *
-     * @param args
-     */
     public static void main(String[] args) {
         SpringApplication.run(MypraApplication.class, args);
     }
--- src/main/java/com/ziv/mypra/package-info.java
+++
@@ -1,4 +0,0 @@
-package com.ziv.mypra;
-/**
- * helloworld
- */
